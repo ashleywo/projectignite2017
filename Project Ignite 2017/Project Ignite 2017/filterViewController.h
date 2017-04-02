16d92a33f55951a943a614f99b4d5c719254b15b
Bronwyn Thomas <bronwynthomas@Bronwyns-Air.wv.cc.cmu.edu>
2017-04-02 16:01:43 -0400

    filter page updates

--- Project Ignite 2017/Project Ignite 2017/filterViewController.h
+++ Project Ignite 2017/Project Ignite 2017/filterViewController.h
@@ -9,14 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "GPUImage.h"
 @interface filterViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
-
+@property (strong, nonatomic) GPUImageOutput<GPUImageInput> *selectedFilter;
 @property (weak, nonatomic) IBOutlet UISlider *filterSlider;
+@property (strong, nonatomic) NSString *filter;
 @property (strong,nonatomic) UIImage *image;
+@property (strong, nonatomic) UIImage *currentImage;
+@property (strong, nonatomic) UIImage *nofilterImage;
 @property (weak, nonatomic) IBOutlet UIButton *backButton;
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageVIewer;
 
+- (IBAction)adjustFilter:(id)sender;
+- (IBAction)filter2:(id)sender;
+
 - (IBAction)filter1:(id)sender;
+- (IBAction)resetFilters:(id)sender;
 
 @end
 
--- Project Ignite 2017/Project Ignite 2017/filterViewController.m
+++ Project Ignite 2017/Project Ignite 2017/filterViewController.m
@@ -18,18 +18,71 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.imageVIewer.image = self.image;
-  
+     self.filterSlider.hidden = YES;
+    self.currentImage = self.image;
+    self.nofilterImage = self.image;
 }
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)adjustFilter:(id)sender {
+    if ([self.filter isEqualToString:@"filter1"])
+    {
+     [(GPUImageBrightnessFilter *)self.selectedFilter setBrightness:[(UISlider *)sender value]];
+    }
+    else if ([self.filter isEqualToString:@"filter2"])
+    {
+     [(GPUImageSharpenFilter *)self.selectedFilter setSharpness:[(UISlider *)sender value]];
+    }
+    self.imageVIewer.image = [self.selectedFilter imageByFilteringImage:self.image];
+    self.currentImage = self.imageVIewer.image;
+}
+
+- (IBAction)filter2:(id)sender {
+    
+    self.image = self.currentImage;
+    
+    self.filterSlider.hidden = NO;
+    [self.filterSlider setValue:0.0];
+    [self.filterSlider setMinimumValue:-4.0];
+    [self.filterSlider setMaximumValue:4.0];
+    
+    self.filter = @"filter2";
+
+    GPUImageFilter *selectedFilter = [[GPUImageSharpenFilter alloc] init];
+    self.selectedFilter = selectedFilter;
+    UIImage *filteredImage = [selectedFilter imageByFilteringImage:self.image];
+    self.image = filteredImage;
+    [self.imageVIewer setImage:self.image];
+
+    
+}
+
 - (IBAction)filter1:(UIButton *)sender {
     
-    GPUImageFilter *filter2 = [[GPUImagePosterizeFilter alloc] init];
-    UIImage *filteredImage = [filter2 imageByFilteringImage:self.image];
+    self.image = self.currentImage;
+    
+    self.filterSlider.hidden = NO;
+    [self.filterSlider setValue:0.0];
+    [self.filterSlider setMinimumValue:-1.0];
+    [self.filterSlider setMaximumValue:1.0];
+    
+    self.filter = @"filter1";
+    
+    GPUImageFilter *selectedFilter = [[GPUImageBrightnessFilter alloc] init];
+    self.selectedFilter = selectedFilter;
+    UIImage *filteredImage = [selectedFilter imageByFilteringImage:self.image];
     self.image = filteredImage;
     [self.imageVIewer setImage:self.image];
 }
+
+- (IBAction)resetFilters:(id)sender {
+    self.filterSlider.hidden = YES;
+    self.imageVIewer.image = self.nofilterImage;
+    
+    }
 
 @end
